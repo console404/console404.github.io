9ec296cb01dfbd7a9b1b4828b99e3f93c686e85b
Dixi <44095720+Dixi18@users.noreply.github.com>
2019-10-16 13:52:45 +0200

    added js and added some other stuff

---
+++ script.js
@@ -0,0 +1,12 @@
+function linkAlertW() {
+    var r = confirm("Do you want to be transferred to Wattpad.com?");
+    if (r == true) {
+        window.open("https://www.wattpad.com/user/console404");
+    }
+}
+function linkAlertI() {
+    var r = confirm("Do you want to be transferred to Instagram.com?");
+    if (r == true) {
+        window.open("https://www.instagram.com/console404");
+    }
+}
