c4840b6a3a10beba7394cc1c740b22483efc07b5
Dixi <44095720+Dixi18@users.noreply.github.com>
2020-03-19 14:36:45 +0100

    1st attempt for cookie stuff

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ function linkAlertI() {
         window.open("https://www.instagram.com/console404");
     }
 }
+var time = new Date();
+time.setTime(time.getTime() + 24 * 60 * 60 * 1000);
+if (activityCheck != "") {
+    alert("Welcome back!");
+    console.log("alert finished");
+}
+document.cookie = "BeenHereRecently=true;path=/;secure;expires=" + time.toUTCString();
+var activityCheck = document.cookie;
+console.log("reached the end");
